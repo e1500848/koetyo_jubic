7959c52be31ab51951dc7da72cfcec98c6a51a6a
e1500848 <kim_somebody@hotmail.com>
2019-01-09 15:51:19 +0200

    added new form

--- src/components/KoeTyo.js
+++ src/components/KoeTyo.js
@@ -1,6 +1,6 @@
 //@flow
 import React from "react";
-import {Chart} from "./Chart"
+import {Bar} from 'react-chartjs-2';
 
 import { connect } from 'react-redux'
 
@@ -19,24 +19,61 @@ class KoeTyo extends React.PureComponent<Props> {
   constructor() {
     super();
     this.state = {
+      showForm: false,
+      chartData:{},
+      data:[
+        {
+        "value":26,
+        "date":'2018-10-30T12:34:21.184Z'
+        },
+        {
+        "value":5,
+        "date":'2018-10-30T09:34:21.184Z'
+        },
+        {
+        "value":3,
+        "date":'2018-10-30T20:34:21.184Z'
+        },
+        {
+        "value":17,
+        "date":'2018-10-30T15:34:21.184Z'
+        }
+      ]
+    }
+
+  }
+
+  componentWillMount() {
+		this.getChartData();
+	}
+
+  getChartData(){
+
+    this.state.data.sort(function (a,b){
+        if(a.date < b.date){
+          return -1;
+        }
+        if(a.date > b.date){
+          return 1;
+        }
+        return 0;
+    });
+
+    var tableValues=[];
+    var tableDates = [];
+
+    this.state.data.map(function(value) {
+        tableValues.push(value.value);
+        tableDates.push(value.date);
+    });
+
+    this.setState({
       chartData:{
-        labels: ['2018-10-30T12:34:21.184Z',
-        '2018-10-30T13:34:21.184Z',
-        '2018-10-30T14:34:21.184Z',
-        '2018-10-30T15:34:21.184Z',
-        '2018-10-30T16:34:21.184Z',
-        '2018-10-30T17:34:21.184Z'],
+        labels:tableDates,
         datasets:[
           {
-            label:'Data',
-            data:[
-              26,
-              59,
-              16,
-              9,
-              73,
-              18
-            ],
+            label:'Value',
+            data:tableValues,
             backgroundColor:[
               'rgba(255, 99, 132, 0.6)',
               'rgba(54, 162, 235, 0.6)',
@@ -49,23 +86,56 @@ class KoeTyo extends React.PureComponent<Props> {
           }
         ]
       }
-    }
+      });
+  }
+
+  addData(e){
+    e.preventDefault();
+
+    const newValue = this.newValue.value;
+    const newDate = this.newDate.value;
+
+    this.setState({
+      data: [...this.state.data, {"value":newValue,"date":newDate}]
+    }, function(){
+      this.getChartData();
+    });
+
+    this.addForm.reset();
+  }
+
+  newForm(){
+    console.log('clicked');
+    return (
+      <form onSubmit={(e) => {this.addData(e)}} ref={input => this.addForm = input}>
+        <input required type="number" id="newValue" ref={input => this.newValue = input} />
+        <input required type="text" id="newDate" ref={input => this.newDate = input} />
+        <button type="submit">Add</button>
+      </form>
+    );
+  }
+
+  _onButtonClick(){
+    this.setState({
+      showForm: true
+    });
   }
 
   render() {
   		return (
         <React.Fragment>
-          <Chart chartData={this.state.chartData} titleText="BAR CHART" legendPosition="bottom"/>
+          <Bar data={this.state.chartData} width={10} height={250} options={{maintainAspectRatio: false}}/>
 
-          <form>
-            <input type="text" id="newValue"/>
-            <input type="text" id="newDate"/>
+          <form onSubmit={(e) => {this.addData(e)}} ref={input => this.addForm = input}>
+            <input required type="number" id="newValue" ref={input => this.newValue = input} />
+            <input required type="text" id="newDate" ref={input => this.newDate = input} />
             <button type="submit">Add</button>
           </form>
+          <button onClick={this._onButtonClick.bind(this)}>new form</button>
+          {this.state.showForm ? this.newForm() : null}
         </React.Fragment>
   		);
   	}
-
 }
 
 const mapStateToProps = (state: ApplicationState) => ({
